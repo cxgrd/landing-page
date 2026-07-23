a44aa9a6243d237399728df16753e84a468555cb
Manan Sharma <manan140306@gmail.com>
2026-07-23 19:46:14 +0530

    wow

--- app/docs/layout.tsx
+++
@@ -1,5 +0,0 @@
-import DocsLayoutClient from './layout-client'
-
-export default function DocsLayout({ children }: { children: React.ReactNode }) {
-  return <DocsLayoutClient>{children}</DocsLayoutClient>
-}
